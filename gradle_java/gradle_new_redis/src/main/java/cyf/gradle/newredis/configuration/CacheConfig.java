af3a20426c7cd18ff4cd0551482f72e3254a0283
chengyufei <chengyufei@daishumovie.com>
2017-10-31 11:46:44 +0800

    rediscachemanager 缓存，方法级缓存，不同dao中的Mapper.xml 缓存

---
+++ gradle_java/gradle_new_redis/src/main/java/cyf/gradle/newredis/configuration/CacheConfig.java
@@ -0,0 +1,83 @@
+package cyf.gradle.newredis.configuration;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Cheng Yufei
+ * @create 2017-10-30 16:32
+ **/
+@Configuration
+@EnableCaching
+public class CacheConfig extends CachingConfigurerSupport {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+
+    private int port;
+
+    private int timeout;
+
+    @Bean
+    public KeyGenerator wiseKeyGenerator() {
+
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    //第二步执行
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+        // 设置 key-value 超时时间 ，在redis中存在时间
+        cacheManager.setDefaultExpiration(60);
+        return cacheManager;
+    }
+
+    //第一步执行
+    @Bean
+    public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        setSerializer(template); //设置序列化工具，这样Bean不需要实现Serializable接口
+        template.afterPropertiesSet();
+        return template;
+
+    }
+
+    private void setSerializer(StringRedisTemplate template) {
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+    }
+}
---
+++ gradle_java/gradle_new_redis/src/main/java/cyf/gradle/newredis/controller/UserController.java
@@ -0,0 +1,25 @@
+package cyf.gradle.newredis.controller;
+
+import cyf.gradle.newredis.module.User;
+import cyf.gradle.newredis.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Cheng Yufei
+ * @create 2017-10-30 17:09
+ **/
+@RestController
+@RequestMapping("/newredis/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/getUser/{id}/{username}/{pwd}")
+    public User getUser(@PathVariable Integer id,@PathVariable String username,@PathVariable String pwd) {
+        return userService.getUser(id,username,pwd);
+    }
+}
