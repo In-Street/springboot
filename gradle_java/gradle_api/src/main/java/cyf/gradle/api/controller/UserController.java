db672c1c5feb21888fcca428a6058ee4635aa0b8
“Cheng <873401580@qq.com>
2017-12-03 12:14:41 +0800

    无效信息删除

--- gradle_java/gradle_api/src/main/java/cyf/gradle/api/controller/UserController.java
+++ gradle_java/gradle_api/src/main/java/cyf/gradle/api/controller/UserController.java
@@ -12,7 +12,6 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
