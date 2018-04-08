a0099d6f5117a18e36d996860090652bfce6e076
chengyufei <chengyufei@daishumovie.com>
2018-04-08 16:05:30 +0800

    webflux

---
+++ gradle_java/webflux/src/main/java/cyf/gradle/webflux/controller/TestController.java
@@ -0,0 +1,8 @@
+package cyf.gradle.webflux.controller;
+
+/**
+ * @author Cheng Yufei
+ * @create 2018-04-08 16:00
+ **/
+public class TestController {
+}
---
+++ gradle_java/webflux/src/main/java/cyf/gradle/webflux/service/TestService.java
@@ -0,0 +1,8 @@
+package cyf.gradle.webflux.service;
+
+/**
+ * @author Cheng Yufei
+ * @create 2018-04-08 16:01
+ **/
+public class TestService {
+}
