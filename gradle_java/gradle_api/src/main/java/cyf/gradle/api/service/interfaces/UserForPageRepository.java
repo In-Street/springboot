7b0eaa2aeead0eca91da251f6f96f2c533904f2f
chengyufei <chengyufei@daishumovie.com>
2018-07-04 14:13:16 +0800

    module 结构调整_1

--- gradle_java/gradle_api/src/main/java/cyf/gradle/api/service/interfaces/UserForPageRepository.java
+++
@@ -1,8 +0,0 @@
-package cyf.gradle.api.service.interfaces;
-
-
-import cyf.gradle.dao.model.User;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserForPageRepository extends PagingAndSortingRepository<User,Integer> {
-}
