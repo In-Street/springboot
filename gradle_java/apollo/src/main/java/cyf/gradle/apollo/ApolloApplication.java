719f659195d23daa25ecb55fc475df723abb0ce8
chengyufei <chengyufei@daishumovie.com>
2018-09-29 18:35:26 +0800

    Apollo

--- gradle_java/apollo/src/main/java/cyf/gradle/apollo/ApolloApplication.java
+++ gradle_java/apollo/src/main/java/cyf/gradle/apollo/ApolloApplication.java
@@ -1,4 +1,4 @@
-package cyf.gradle.interview;
+package cyf.gradle.apollo;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.beans.factory.support.DefaultBeanNameGenerator;
@@ -6,19 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * boot入口
- *
  */
 @SpringBootApplication(
-        scanBasePackages = {"cyf.gradle.interview"}
+        scanBasePackages = {"cyf.gradle.apollo"}, exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class}
 )
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableApolloConfig
-public class InterviewApplication {
-
+public class ApolloApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(InterviewApplication.class)
+        new SpringApplicationBuilder(ApolloApplication.class)
                 //类名重复bean的处理
                 .beanNameGenerator(new DefaultBeanNameGenerator())
                 .run(args);
