ac284b2502adb55bb9fb4d96389954416c4a7c17
chengyufei <chengyufei@daishumovie.com>
2018-11-20 18:26:46 +0800

    test - 回顾

--- gradle_java/api/src/test/java/TestHome.java
+++ gradle_java/api/src/test/java/TestHome.java
@@ -447,3 +447,34 @@ public class TestHome {
 
 
 }
+
+/**
+ * 回顾点：
+ * BiFunction
+ * getAndIncrement
+ * BeanUtils.copyProperties
+ * 深克隆、浅克隆
+ *localDate.plus(1, ChronoUnit.WEEKS)
+ * LocalTime
+ * DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+ * LocalDateTime
+ * Date date = Date.from(from.atZone(ZoneId.systemDefault()).toInstant());
+ * LocalDate localDate1 = LocalDate.from(date2.toInstant().atZone(ZoneId.systemDefault()));
+ *  ChronoUnit.DAYS.between(from, time);
+ *  Optional.ofNullable  orElse  ifPresent
+ *  stream flatMap
+ *  Multiset
+ *  HashMultimap
+ *  HashBiMap
+ *  HashBasedTable
+ *  Joiner Splitter
+ *  ImmutableMap.of("B", 1, "C", null);
+ *  Iterables.filter transform any concat frequency elementsEqual
+ *  Sets.difference union  intersection
+ *  EnumUtils
+ *  Collections.max
+ *  Ordering.natural().greatestOf(integers, 2);
+ *  Range.closed
+ *  Lists.partition
+ *
+ */
