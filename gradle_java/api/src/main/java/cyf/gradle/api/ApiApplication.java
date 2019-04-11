dd67d63c1694b442bde4bc64eeb56b516043daf9
chengyufei <chengyufei@daishumovie.com>
2019-04-11 16:10:06 +0800

    api - spring cloud alibaba sentinel

--- gradle_java/api/src/main/java/cyf/gradle/api/ApiApplication.java
+++ gradle_java/api/src/main/java/cyf/gradle/api/ApiApplication.java
@@ -49,7 +49,7 @@ import java.util.ArrayList;
 public class ApiApplication {
 
     public static void main(String[] args) {
-        //initSentinelRule();
+        initSentinelRule();
         new SpringApplicationBuilder(ApiApplication.class)
                 //类名重复bean的处理
                 .beanNameGenerator(new DefaultBeanNameGenerator())
@@ -92,7 +92,7 @@ public class ApiApplication {
     private static void initSentinelRule() {
         ArrayList<FlowRule> flowRules = new ArrayList<>();
         FlowRule rule = new FlowRule();
-        rule.setRefResource("userByName");
+        rule.setRefResource("ByName");
         //此处添加流控未生效
         rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
         rule.setCount(1);
