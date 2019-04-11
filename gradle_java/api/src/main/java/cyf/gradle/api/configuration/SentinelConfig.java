8d5a379c53c30f6bab9c722d99d1d73cbfa2f895
chengyufei <chengyufei@daishumovie.com>
2019-04-11 16:38:32 +0800

    api - spring cloud alibaba sentinel

--- gradle_java/api/src/main/java/cyf/gradle/api/configuration/SentinelConfig.java
+++ gradle_java/api/src/main/java/cyf/gradle/api/configuration/SentinelConfig.java
@@ -8,12 +8,13 @@ import org.springframework.context.annotation.Configuration;
  * @author Cheng Yufei
  * @create 2019-03-22 16:38
  *
+ * sentinel-annotation-aspectj 方式需添加 ； spring-cloud-starter-alibaba-sentinel 方式无需添加
  * 注解限流配置
  **/
-//@Configuration
+@Configuration
 public class SentinelConfig {
 
-    //@Bean
+    @Bean
     public SentinelResourceAspect sentinelResourceAspect() {
         return new SentinelResourceAspect();
     }
